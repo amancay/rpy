30bfd2c459976b290e378675eb21c3258c9fc625
Alejandro Engelmann <alejandro.engelmann@slu.se>
2022-08-07 06:44:23 +0200

    cleanup

--- sws/t1.py
+++ sws/t1.py
@@ -55,10 +55,6 @@ with open('/home/alejeng/test.html', 'w') as f:
 import webbrowser
 new = 2 # open in a new tab, if possible
 
-# open a public URL, in this case, the webbrowser docs
-url = "http://docs.python.org/library/webbrowser.html"
-webbrowser.open(url,new=new)
-
 # open an HTML file on my own (Windows) computer
 url = "file:///home/alejeng/test.html"
 webbrowser.open(url,new=new)
